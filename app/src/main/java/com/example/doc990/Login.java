9604dacdcc6395c053b77c8647739b854fbb9c65
shehai <wshehaniif@gmail.com>
2020-10-06 21:50:46 +0530

    Create user login

---
+++ app/src/main/java/com/example/doc990/Login.java
@@ -0,0 +1,17 @@
+package com.example.doc990;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
