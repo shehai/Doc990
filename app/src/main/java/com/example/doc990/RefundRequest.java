678d5849de1289178bda12edfc81aa7b3a61f901
GayaliMethmini <gayali.methmini@gmail.com>
2020-10-09 04:02:50 +0530

    Create Refund Request page

---
+++ app/src/main/java/com/example/doc990/RefundRequest.java
@@ -0,0 +1,18 @@
+package com.example.doc990;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class RefundRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_refund_request);
+    }
+}
