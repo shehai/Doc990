5258862975d9f59e84112423a5c3e41c9c9e89df
GayaliMethmini <gayali.methmini@gmail.com>
2020-10-08 19:14:14 +0530

    Create Check Ongoing Number page

---
+++ app/src/main/java/com/example/doc990/CheckOngoingNum.java
@@ -0,0 +1,18 @@
+package com.example.doc990;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class CheckOngoingNum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_check_ongoing_num);
+    }
+}
