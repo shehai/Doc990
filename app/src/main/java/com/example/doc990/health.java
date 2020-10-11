bb4cdc837c5d286a9c8594ca156ebda71e40e450
shehai <wshehaniif@gmail.com>
2020-10-11 14:14:22 +0530

    shranjan files

---
+++ app/src/main/java/com/example/doc990/health.java
@@ -0,0 +1,14 @@
+package com.example.doc990;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class health extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+    }
+}
--- app/src/main/java/com/example/doc990/home.java
+++ app/src/main/java/com/example/doc990/home.java
@@ -1,12 +1,20 @@
 package com.example.doc990;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.doc990.ui.main.HomeFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class home extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,44 @@ public class home extends AppCompatActivity {
                     .replace(R.id.container, HomeFragment.newInstance())
                     .commitNow();
         }
+
+        dl = (DrawerLayout)findViewById(R.id.container);
+        t = new ActionBarDrawerToggle(this, dl,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+               /* switch(id)
+                {
+                    case R.id.account:
+
+                        return true;
+                }*/
+
+
+                return true;
+            }
+        } );
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
