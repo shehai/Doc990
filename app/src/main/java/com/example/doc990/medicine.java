c81a4f5c8ebc4ec6edb733f912b207f5842c64d3
HP <harshamadushan975@gmail.com>
2020-10-24 10:13:56 +0530

    Monti final phase - commit 01

--- app/src/main/java/com/example/doc990/medicine.java
+++ app/src/main/java/com/example/doc990/medicine.java
@@ -1,13 +1,18 @@
 package com.example.doc990;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class medicine extends AppCompatActivity {
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,29 @@ public class medicine extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_medicine);
+
+        submit = findViewById(R.id.submit_btn);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                Toast.makeText(medicine.this, "Success",Toast.LENGTH_LONG).show();
+
+                Intent d = new Intent(medicine.this,Main2Activity.class);
+                startActivity(d);
+
+            }
+        });
+
     }
-    public void click(View v){
+ /*   public void click(View v){
+
+        Toast.makeText(this, "Success",Toast.LENGTH_LONG).show();
+
         Intent d = new Intent(this,Main2Activity.class);
         startActivity(d);
-    }
+    }*/
+
+
+
 }
